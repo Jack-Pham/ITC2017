00ad86d18b4690faa67ee28323df07e86197edbe
Exia <onevarez@cpp.edu>
2017-04-21 09:56:33 -0700

    added review score and distance information

--- public/js/amenities.js
+++ public/js/amenities.js
@@ -49,14 +49,18 @@ function processData(data,status,xhr){
     for (var x = 0; x < results; x++){
         latLng = {lat: data[x].coordinates.latitude, lng: data[x].coordinates.longitude};
         
-        makeMarker(latLng, data[x].name, data[x].distance, data[x].location, data[x].url);
+        makeMarker(latLng, data[x].name, data[x].distance, data[x].rating, data[x].location, data[x].url);
         
     }
 
 }
 
-function makeMarker(coordinates, title, distance, location, yelpUrl){
+function makeMarker(coordinates, title, distance, rating, location, yelpUrl){
+    var distance = distance / 1609.34;
+    var distance = distance.toFixed(2);
     var content = '<div style="color: #000000;" ><strong>' + title + '</strong><br>' + location.address1 + '<br>' + location.city +', '+ location.state +
+                  '<br> Distance: ' + distance + ' miles' +
+                  '<br> Rating: ' + rating +
                   '<br> View on <a href=' + yelpUrl +' target=_blank> yelp</a> </div>'
 
     marker = new google.maps.Marker({
