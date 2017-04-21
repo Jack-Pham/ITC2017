daca4b671f345ad0c71fc10395882d7d497bdfc8
Jack-Pham <phminhthong88@gmail.com>
2017-04-21 04:11:49 -0700

    Update

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET home page */
 router.get('/', function(req, res) {
     res.render('home', {title: "New Home"});
 });
+
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var spotcrime = require('spotcrime');
 
 /* GET index page */
 router.get('/', function(req, res) {
@@ -8,6 +10,22 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
     res.redirect('/home');
+});
+
+router.post('/crime',function(req, res, next) {
+
+		var data = req.body;
+		var loc = {
+		  lat: data.lat,
+		  lon: data.lng
+		};
+
+		var radius = 0.1; // this is miles
+
+		spotcrime.getCrimes(loc, radius, function(err, crimes){
+				res.send(crimes);
+		});
+
 });
 
 module.exports = router;
